6906d69b0c9aa4be2c2d4452868afeecac1b90f2
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-19 16:41:31 +1100

    Test widgets in shiny setup

--- shiny_setup.R
+++ shiny_setup.R
@@ -1,5 +1,11 @@
 # Start of shiny app
 library(shiny)
-server = function(input, output, session) {} # the server
-ui = basicPage("start of app") # the user interface
+
+server <- function(input, output, session) {} # the server
+
+ui <- basicPage(
+  h1("Testing widgets"),
+  textInput("mystring", "write here"),
+  checkboxInput("mycheckbox", "Test Factor")) # the user interface
+
 shinyApp(ui = ui, server = server) # app launch

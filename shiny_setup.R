29d64b66f6ffb5904f850ce844eb0cc089c5ee2f
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-19 17:15:20 +1100

    Check shiny app layout properties

--- shiny_setup.R
+++ shiny_setup.R
@@ -1,23 +1,14 @@
 # Start of shiny app
 library(shiny)
 
-server <- function(input, output, session) {  # the server
-  
-  data <- reactive({
-    rnorm(50) * input$myslider
-  })
-  
-  output$plot <- renderPlot({
-    plot(data(), col= "red", pch = 21, bty = "n")
-  })
-} 
+server <- function(input, output, session) {}  # the server 
 
-ui <- basicPage(                              # the user interface
-  h1("Integrating input widgets with server test, reactive & renderPlot"),
-  sliderInput(inputId = "myslider",
-              label = "Slider1",
-              value = 1, min = 1, max = 20),
-  plotOutput("plot")
+ui <- fluidPage(                              # the user interface
+  
+  sidebarLayout(
+    sidebarPanel("my sidebar"),
+    mainPanel("my mainpanel")
+  )
 )
   
 shinyApp(ui = ui, server = server) # app launch

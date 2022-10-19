177e36366458e5acc356b0c943be3044ef5a4d31
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-19 17:02:05 +1100

    Integrate input widgets with server, test 2

--- shiny_setup.R
+++ shiny_setup.R
@@ -2,16 +2,22 @@
 library(shiny)
 
 server <- function(input, output, session) {  # the server
-  observe({
-    addtext <- paste("your initial input:", input$mystring)
-    updateTextInput(session, "mystring2", value=addtext)
+  
+  data <- reactive({
+    rnorm(50) * input$myslider
+  })
+  
+  output$plot <- renderPlot({
+    plot(data(), col= "red", pch = 21, bty = "n")
   })
 } 
 
 ui <- basicPage(                              # the user interface
-  h1("Integrating input widgets with server test"),
-  textInput("mystring", "write here"),
-  textInput("mystring2", "Full App Output")
+  h1("Integrating input widgets with server test, reactive & renderPlot"),
+  sliderInput(inputId = "myslider",
+              label = "Slider1",
+              value = 1, min = 1, max = 20),
+  plotOutput("plot")
 )
   
 shinyApp(ui = ui, server = server) # app launch

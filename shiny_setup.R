1e8fe4f8e46668870bbbd32faef2da4205a731ec
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-19 17:20:27 +1100

    Check shiny app multi-page layout properties

--- shiny_setup.R
+++ shiny_setup.R
@@ -6,8 +6,18 @@ server <- function(input, output, session) {}  # the server
 ui <- fluidPage(                              # the user interface
   
   sidebarLayout(
-    sidebarPanel("my sidebar"),
-    mainPanel("my mainpanel")
+    sidebarPanel(
+      sliderInput(inputId = "s1",
+                  label = "My Slider",
+                  value = 1, min = 1, max = 20)
+    )
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Tab1", "First Tab"),
+        tabPanel("Tab2", "Second Tab"),
+        tabPanel("Tab3", "Third Tab")
+      )
+    )
   )
 )
   

1f55f1f4054f6b7115da881b31e5a3a244d52094
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-19 16:52:47 +1100

    Integrate input widgets with server test

--- shiny_setup.R
+++ shiny_setup.R
@@ -1,11 +1,17 @@
 # Start of shiny app
 library(shiny)
 
-server <- function(input, output, session) {} # the server
+server <- function(input, output, session) {  # the server
+  observe({
+    addtext <- paste("your initial input:", input$mystring)
+    updateTextInput(session, "mystring2", value=addtext)
+  })
+} 
 
-ui <- basicPage(
-  h1("Testing widgets"),
+ui <- basicPage(                              # the user interface
+  h1("Integrating input widgets with server test"),
   textInput("mystring", "write here"),
-  checkboxInput("mycheckbox", "Test Factor")) # the user interface
-
+  textInput("mystring2", "Full App Output")
+)
+  
 shinyApp(ui = ui, server = server) # app launch

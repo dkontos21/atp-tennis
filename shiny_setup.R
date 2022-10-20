398f90e814eadefd8070ded9149b0dc93dcca7c6
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-20 12:30:25 +1100

    Test out in app data table

--- shiny_setup.R
+++ shiny_setup.R
@@ -1,27 +1,18 @@
-# Start of shiny app
+## Simple datatable
 library(shiny)
+library(ggplot2)
+library(DT)
 
-server <- function(input, output, session) {}  # the server 
+server <- function(input, output, session) {
+  output$tableDT <- DT::renderDataTable(diamonds[1:1000,], 
+                                        options = list(paging=F), 
+                                        rownames=F, 
+                                        filter = "top")
+}
 
-ui <- fluidPage(themeSelector(), #test out Shinythemes in the user interface
-  
-  titlePanel(strong("Multi-page app layout testing")),
-  
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(inputId = "s1",
-                  label = "My Slider",
-                  value = 1, min = 1, max = 20)
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Tab1", "Weblinks with direct tag a()", a(href="https://github.com/serve-and-volley/atp-world-tour-tennis-data.git", "serve-and-volley's GitHub repo")),
-        tabPanel("Tab2", "Second Tab"),
-        tabPanel("Tab3", "Third Tab")
-      )
-    )
-  )
+ui <- fluidPage(
+  DT::dataTableOutput("tableDT")
 )
-  
-shinyApp(ui = ui, server = server) # app launch
+
+shinyApp(ui = ui, server = server)
+

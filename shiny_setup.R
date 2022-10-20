f5b646676d008d820cc3925afd81065e27b6f768
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-20 12:34:09 +1100

    Test in-app data table refinements

--- shiny_setup.R
+++ shiny_setup.R
@@ -1,13 +1,22 @@
-## Simple datatable
+## Refining datatable
 library(shiny)
 library(ggplot2)
 library(DT)
 
 server <- function(input, output, session) {
-  output$tableDT <- DT::renderDataTable(diamonds[1:1000,], 
-                                        options = list(paging=F), 
-                                        rownames=F, 
-                                        filter = "top")
+  output$tableDT <- DT::renderDataTable(datatable(diamonds[1:1000,],
+                                                  options = list(paging=F), 
+                                                  rownames=F, 
+                                                  filter = "top") %>% 
+                                          formatCurrency("price", "$") %>%
+                                          formatStyle("price", color = "green") %>%
+                                          formatStyle("cut",
+                                                      transform = "rotateX(20deg) rotateY(5deg) rotateZ(5deg)",
+                                                      backgroundColor = styleEqual(
+                                                        unique(diamonds$cut), c("salmon", "lightblue", "grey", "lightgreen", "lightpink")
+                                                      )
+                                          )
+  ) 
 }
 
 ui <- fluidPage(

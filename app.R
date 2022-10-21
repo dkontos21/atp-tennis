24e272aef2482d1236036dcb296b4862c7f4b3d5
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-21 13:27:47 +1100

    Update: interactive map and data table tabs

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 
 library(shiny)
 library(shinythemes)
+library(shinyWidgets)
 library(DT)
 library(leaflet)
 library(tidyverse)
@@ -22,50 +23,22 @@ colnames(tournaments) <- c("tourney_year_id", "tourney_order", "tourney_type",
 
 attach(tournaments)
 
-server <- function(input, output,session){
-  
-  # interactive map
-  output$map <- renderLeaflet({
-    leaflet(tournaments) %>%
-      addCircleMarkers(~tourney_latitude, ~tourney_longitude,
-                 label = ~tourney_name,
-                 labelOptions = labelOptions(textsize = "12px"),
-                 popup = "Test")
-  })
-  
-  observe({
-    leafletProxy("map", data = tournaments) %>%
-      clearShapes() %>%
-      addCircleMarkers(~tourney_latitude, ~tourney_longitude,
-                       label = ~tourney_name,
-                       labelOptions = labelOptions(textsize = "12px"),
-                       popup = "Test")
-  })
-  
-  # table for the Data table tab
-  output$tableDT <- DT::renderDataTable(DT::datatable(tournaments))
-}
-
-
-# Interface
-
-# Choices for drop-downs
-vars <- c()
-
 ui <- navbarPage(theme = shinytheme("sandstone"), title = h3("Tournaments of the ATP Tour"),
-                 tabPanel("Interactive map",
-                   
-                   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-                   
-                   leafletOutput("map", width = "100%", height = "100%"),
-                   
-                   downloadButton(outputId = "mydownload", label = "Download Table")
-                 ),
-                 
+                 tabPanel("Interactive Map",
+                          leafletOutput('myMap')),
                  tabPanel("Documentation"),
                  
                  tabPanel("Data Table with the underlying data",
                           DT::dataTableOutput("tableDT"))
 )
 
+server = function(input, output,session) {
+  map = leaflet() %>% addTiles() %>% setView(-93.65, 42.0285, zoom = 3)
+  output$myMap = renderLeaflet(map)
+  
+  # table for the Data table tab
+  output$tableDT <- DT::renderDataTable(DT::datatable(tournaments))
+}
+
+shinyApp(ui = ui, server = server)
 shinyApp(ui = ui, server = server)

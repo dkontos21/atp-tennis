c1094ea359e611614bcc7065c2c590689f864f1e
Dimitri Kontos <dkontos.unimelb@gmail.com>
2022-10-21 13:23:29 +1100

    Update multi-page layout with interactive map

--- basic_map_shiny.R
+++ basic_map_shiny.R
@@ -3,8 +3,16 @@ library(shiny)
 library(shinyWidgets)
 library(leaflet)
 library(tidyverse)
+library(shinythemes)
 
-ui <- fluidPage(leafletOutput('myMap'))
+ui <- navbarPage(theme = shinytheme("sandstone"), title = h3("Tournaments of the ATP Tour"),
+                 tabPanel("Interactive Map",
+                          leafletOutput('myMap')),
+                 tabPanel("Documentation"),
+                 
+                 tabPanel("Data Table with the underlying data")
+)
+  
 server = function(input, output,session) {
   map = leaflet() %>% addTiles() %>% setView(-93.65, 42.0285, zoom = 3)
   output$myMap = renderLeaflet(map)
